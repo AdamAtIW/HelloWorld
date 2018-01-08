f7ebd33efd263874faf3fb2d5a6737a0c302ae79
Adam Faulkner <adam.faulkner@integrationworks.local>
2018-01-09 11:38:52 +1300

    all off

--- Hello_World.java
+++ Hello_World.java
@@ -4,7 +4,7 @@ public class Hello_World {
 	public static void main(String[] args) {
 		for(int i = 0; i< 5; i++) {
 			
-			System.out.println("Hello young Anakin non deprecated ....  " + i);
+			System.out.println("all off ....  " + i);
 
 		}
 		
