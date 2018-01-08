f6ebfa1e014a483e7da74091fcc951bcb64af311
Adam Faulkner <adam.faulkner@integrationworks.local>
2018-01-09 11:51:38 +1300

    git webhook

--- Hello_World.java
+++ Hello_World.java
@@ -4,7 +4,7 @@ public class Hello_World {
 	public static void main(String[] args) {
 		for(int i = 0; i< 5; i++) {
 			
-			System.out.println("all off ....  " + i);
+			System.out.println("git web hook  " + i);
 
 		}
 		
