f23ea7d682fb2de21d3c9482fe7c5aea0940a8da
Adam Faulkner <adam.faulkner@integrationworks.local>
2018-01-09 13:31:14 +1300

    monday

--- Hello_World.java
+++ Hello_World.java
@@ -4,7 +4,7 @@ public class Hello_World {
 	public static void main(String[] args) {
 		for(int i = 0; i< 5; i++) {
 			
-			System.out.println("im running out of ideas " + i);
+			System.out.println("monday " + i);
 
 		}
 		
