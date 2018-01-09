56ceecbc6e06ecdfcb91c561dfbea1102e506350
Adam Faulkner <adam.faulkner@integrationworks.local>
2018-01-09 13:15:23 +1300

    auto

--- Hello_World.java
+++ Hello_World.java
@@ -4,7 +4,7 @@ public class Hello_World {
 	public static void main(String[] args) {
 		for(int i = 0; i< 5; i++) {
 			
-			System.out.println("hello tom please wrk for me" + i);
+			System.out.println("auto" + i);
 
 		}
 		
